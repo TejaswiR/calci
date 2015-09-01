3f6bf8da1f3ce69f43ba61c21983597b130c3eb7
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-09-01 16:14:52 +0530

    WIP

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+              sass: {                              // Task 
+                 dist: {                            // Target 
+                   files: {                         // Dictionary of files 
+                    'assets/stylesheets/styles.css': 'assets/stylesheets/src/styles.scss'
+                    }
+                }
+              },
+              watch:   {
+                css: {
+                 files: ['assets/stylesheets/src/*.scss'],
+                 tasks: ['sass', 'cssmin'],
+               }
+             },
+            cssmin: {
+              target: {
+                files: {
+                  'assets/stylesheets/app.min.css' : 
+                  [
+                  'assets/stylesheets/bootstrap.css',
+                  'assets/stylesheets/styles.css'
+                 ],
+                }
+              }
+            }
+          });
+
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default',['sass','cssmin','watch']);
+};
