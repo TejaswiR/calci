0ce459106b7f1067c87817de8ddc1ec17e760b2a
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-09-04 13:14:39 +0530

    bugfix

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
   
   ['0','1','2','3','4','5','6','7','8','9'].forEach (function(digit){
     $(document).bind('keyup',digit, function(){
-    handleInput();
+    handleInput(digit);
     });
   });
 });
