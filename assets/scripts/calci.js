2c7b0a564114665edf329f03e910fc8bedbfd227
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-09-04 12:51:41 +0530

    user can now enter digits from keyboard

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,21 +1,34 @@
 $(document).ready(function(){
- $('#calculator .input').click (function(){
-   var input;
-  if(this.dataset.keyType == "digit"){
-    input = this.dataset.digit;
+  $('#calculator .input').click (function(){
+
+    var input;
+    
+    if(this.dataset.keyType == "digit"){
+      input = this.dataset.digit;
+    
     } else if(this.dataset.keyType == "operator") {
-    input = this.dataset.operator;
-  }else if(this.dataset.keyType == "delete"){
-  $("#preview").html($("#preview").html().slice(0, -1));
+      input = this.dataset.operator;
+    
+    }else if(this.dataset.keyType == "delete"){
+      $("#preview").html($("#preview").html().slice(0, -1));
+    
     }else if(this.dataset.keyType == "equals"){
-    $("#result").html(eval($("#preview").html()));
-    }
+      $("#result").html(eval($("#preview").html()));
+  }
+    
     if(typeof(input)!= "undefined"){
- $("#preview").html($("#preview").html()+ input);
-    }
-  });
- $("#calculator #delete").dblclick(function(){
-  $("#preview").html("");
-  $("#result").html("");
- });
+      $("#preview").html($("#preview").html()+ input);
+  }
 });
+
+  $("#calculator #delete").dblclick(function(){
+    $("#preview").html("");
+    $("#result").html("");
+});
+  
+  ['0','1','2','3','4','5','6','7','8','9'].forEach (function(digit){
+    $(document).bind('keyup',digit, function(){
+    $("#preview").html($("#preview").html()+ digit);
+      });
+    });
+  });
