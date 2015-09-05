9e0ae54d073708c616e3c3e35c5bec107336dd9c
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-09-05 20:58:42 +0530

    modularising js file

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,40 +1,41 @@
-function handleInput(input) {
-  $("#preview").html($("#preview").html()+ input);
-}
-
-function handleDelete (){
-  $("#preview").html($("#preview").html().slice(0, -1));
-}
- function evaluateResult (){
-  $("#result").html(eval($("#preview").html()));
-}
-
-$(document).ready(function(){
+var Calci = {
+ init: function(){
   $('#calculator .input').click (function(){
-
-    var input;
-    
     if(this.dataset.keyType == "digit"){
-      handleInput(this.dataset.digit);
+      Calci.handleInput(this.dataset.digit);
     
     } else if(this.dataset.keyType == "operator") {
-      handleInput(this.dataset.operator);
+      Calci.handleInput(this.dataset.operator);
     
     }else if(this.dataset.keyType == "delete"){
-     handleDelete ();
+     Calci.handleDelete ();
     }else if(this.dataset.keyType == "equals"){
-      evaluateResult ();
-   }
+      Calci.evaluateResult ();
+     }
  });
-
-  $("#calculator #delete").dblclick(function(){
-    $("#preview").html("");
-    $("#result").html("");
+    $("#calculator #delete").dblclick(function(){
+      $("#preview").html("");
+      $("#result").html("");
   });
-  
-  ['0','1','2','3','4','5','6','7','8','9'].forEach (function(digit){
-    $(document).bind('keyup',digit, function(){
-    handleInput(digit);
+    ['0','1','2','3','4','5','6','7','8','9'].forEach (function(digit){
+      $(document).bind('keyup',digit, function(){
+      Calci.handleInput(digit);
+      });
     });
-  });
+},
+
+ handleInput: function(input) {
+  $("#preview").html($("#preview").html()+ input);
+},
+
+ handleDelete: function  (){
+  $("#preview").html($("#preview").html().slice(0, -1));
+},
+ evaluateResult: function (){
+  $("#result").html(eval($("#preview").html()));
+  }
+};
+  
+  $(document).ready(function(){
+  Calci.init();
 });
